c519adac88c842becf8be28bc51446f605cca481
Elliot Wood <43952853+ElliotWood13@users.noreply.github.com>
2024-08-06 19:56:51 +0100

    Extract colors from elevated button into main

--- lib/components/elevated_button.dart
+++ lib/components/elevated_button.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class CustomElevatedButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
-  final VoidCallback? onReleased;
   final bool disabled;
   final Icon? icon;
 
@@ -13,65 +12,38 @@ class CustomElevatedButton extends StatelessWidget {
     required this.onPressed,
     this.disabled = false,
     this.icon,
-    this.onReleased,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTapDown: (_) => onPressed(),
-        onTapUp: (_) {
-          if (onReleased != null) {
-            onReleased!();
-          }
-        },
-        onTapCancel: () {
-          if (onReleased != null) {
-            onReleased!();
-          }
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: disabled == false ? Colors.black : Colors.grey.shade700,
-                offset: Offset(2, 2),
-                blurRadius: 0,
-                spreadRadius: 0,
-              ),
-            ],
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: ElevatedButton(
-            onPressed: onPressed,
-            style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.all(disabled == false
-                  ? Color.fromRGBO(177, 255, 158, 1)
-                  : Color.fromRGBO(228, 239, 224, 1)),
-              foregroundColor: WidgetStateProperty.all(
-                  disabled == false ? Colors.black : Colors.grey[700]),
-              padding: WidgetStateProperty.all(
-                  EdgeInsets.symmetric(vertical: 16, horizontal: 32)),
-              shape: WidgetStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-                side: BorderSide(
-                    color:
-                        disabled == false ? Colors.black : Colors.grey.shade700,
-                    width: 3),
-              )),
-              elevation: WidgetStateProperty.all(0),
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: disabled ? Colors.grey.shade700 : Colors.black,
+              offset: Offset(2, 2),
+              blurRadius: 0,
+              spreadRadius: 0,
             ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (icon != null) ...[
-                  icon!,
-                  SizedBox(width: 8),
-                ],
-                Text(text),
+          ],
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: ElevatedButton(
+          onPressed: disabled ? null : onPressed,
+          style: Theme.of(context).elevatedButtonTheme.style,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (icon != null) ...[
+                icon!,
+                SizedBox(width: 8),
               ],
-            ),
+              Text(text),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,32 @@ class MyApp extends StatelessWidget {
             bodyMedium: TextStyle(fontSize: 16, color: Colors.black),
             labelLarge: TextStyle(fontSize: 16, color: Colors.black),
           ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.resolveWith((states) {
+                if (states.contains(WidgetState.disabled)) {
+                  return Color.fromRGBO(228, 239, 224, 1);
+                }
+                return Color.fromRGBO(177, 255, 158, 1);
+              }),
+              foregroundColor: WidgetStateProperty.resolveWith((states) {
+                if (states.contains(WidgetState.disabled)) {
+                  return Colors.grey[700];
+                }
+                return Colors.black;
+              }),
+              padding: WidgetStateProperty.all(
+                  EdgeInsets.symmetric(vertical: 16, horizontal: 32)),
+              shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+                side: BorderSide(
+                  color: Colors.black,
+                  width: 3,
+                ),
+              )),
+              elevation: WidgetStateProperty.all(0),
+            ),
+          ),
         ),
         home: MyHomePage(),
       ),
