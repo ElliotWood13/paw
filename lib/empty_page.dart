2bf468007b15935b47c43cbbb81eb2e34bbf65c6
Elliot Wood <43952853+ElliotWood13@users.noreply.github.com>
2024-08-16 10:30:47 +0100

    Get graphql working with dummy data, add dummy blogs to empty page

--- lib/empty_page.dart
+++ lib/empty_page.dart
@@ -1,19 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+// TODO: Rename empty page
+// TODO: Create header with logo that can be used everywhere
 
 class EmptyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
+    return Stack(
+      children: [
+        Positioned(
+          top: 16.0,
+          left: 16.0,
+          child: Image.asset(
             'assets/logo.png',
-            width: 200.0,
-            height: 200.0,
-          )
-        ],
-      ),
+            width: 50.0,
+            height: 50.0,
+          ),
+        ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Query(
+                    options: QueryOptions(
+                      document: gql(
+                        r'''
+                      query GetPosts {
+                        posts {
+                          id
+                          publishedAt
+                          slug
+                          title
+                          excerpt
+                          content {
+                            text
+                          }
+                          coverImage {
+                            url
+                          }
+                        }
+                      }
+                    ''',
+                      ),
+                    ),
+                    builder: (QueryResult result,
+                        {VoidCallback? refetch, FetchMore? fetchMore}) {
+                      if (result.hasException) {
+                        return Text(result.exception.toString());
+                      }
+
+                      if (result.isLoading) {
+                        return CircularProgressIndicator();
+                      }
+
+                      final List posts = result.data?['posts'] ?? [];
+
+                      if (posts.isEmpty) {
+                        return Text('No posts found.');
+                      }
+
+                      return ListView.builder(
+                        itemCount: posts.length,
+                        itemBuilder: (context, index) {
+                          final post = posts[index];
+
+                          return GestureDetector(
+                            onTap: () {
+                              // Handle tap to navigate to the full blog view
+                            },
+                            child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                                side: BorderSide(color: Colors.black, width: 4),
+                              ),
+                              clipBehavior: Clip.antiAlias,
+                              elevation: 2.0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  if (post['coverImage']?['url'] != null)
+                                    Image.network(
+                                      post['coverImage']['url'],
+                                      width: double.infinity,
+                                      height: 200.0,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          post['title'] ?? 'No Title',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headlineSmall,
+                                        ),
+                                        SizedBox(height: 8.0),
+                                        Text(
+                                          post['publishedAt'] ??
+                                              'No Publication Date',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodySmall,
+                                        ),
+                                        SizedBox(height: 8.0),
+                                        Text(
+                                          post['excerpt'] ?? 'No Excerpt',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyMedium,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:provider/provider.dart';
 import 'home_page.dart';
 import 'state/my_app_state.dart';
+
+final HttpLink httpLink = HttpLink(
+  'https://eu-west-2.cdn.hygraph.com/content/clzl2vxz601on07ut46mx17ev/master',
+);
+
+final AuthLink authLink = AuthLink(
+  getToken: () async =>
+      'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE3MjMxMDk1MzQsImF1ZCI6WyJodHRwczovL2FwaS1ldS13ZXN0LTIuaHlncmFwaC5jb20vdjIvY2x6bDJ2eHo2MDFvbjA3dXQ0Nm14MTdldi9tYXN0ZXIiLCJtYW5hZ2VtZW50LW5leHQuZ3JhcGhjbXMuY29tIl0sImlzcyI6Imh0dHBzOi8vbWFuYWdlbWVudC1ldS13ZXN0LTIuaHlncmFwaC5jb20vIiwic3ViIjoiZjIyZDRlMTYtMjk2Ny00YzQ4LWI1YTctYmM1ZGViZWM4NDliIiwianRpIjoiY2thNWoyZW9iMDN0YzAxd2gwZGZkNjdyeSJ9.SNWKtJ-P1p7VoVT-MmxnIWvEEEQ1E7mg2Z6OH_3kpbgkHOnosCdRQ3qpqfrZRakkkytClO8FH892L385n9xY6rveIOre4-zddBoxGJbRGykb_8VBVGLgRvVe_JZt-V44NS5ibbJEGCKjTMMXEO7jURNSx7ZGOj_bTW6PM_dmMDRdzg_ivQDT9ktPW4A2Wuy-jdWadNL-botOjBib6vI-RLSkmj-KdXTaT-GORSq3-9oPuJZK0UmMrrAiPKL4UdJimZcbLdTM4hbjdiwYKR1TtydFuslZ5dOP-l1fMhlq9B9FLud9Ins0Tr7xpVQUcIy8HcTYcfHI4o7_Xfn037R6z_dEE3vrUhD0MreYnx7v5ucvNMVTawg8fBKsZQbpCE897BfRv2vIyasHVkuEfeQ8zqaXcdZ7m28XPYWGs0Uoxsc5GryZPfFggkpUEUoGcaWpVCjIhSGGubMJU2CkKuP0zULEDVpS3KWBL8osG1o-ULilBRnMQngIMyQfpB0e0jGKV07pqj_MvOh6zVd5i1PW7je-xWYfRd3CX2cCLACFP1Cyll7PiC8Mndur9m42RaRgpSFenvb7PqhEffEX-EAjwlA25nwFt-8S_4jwsv2v7pMiMleBhZMtEm2EPLuc46i7MWmlrdos5e4sCAAFqZsePwTf4-jvMEkItMRNRT1wO3U',
+);
+
+final Link link = authLink.concat(httpLink);
+
+final ValueNotifier<GraphQLClient> client = ValueNotifier(
+  GraphQLClient(
+    link: link,
+    cache: GraphQLCache(store: InMemoryStore()),
+  ),
+);
 
 void main() {
   runApp(MyApp());
@@ -12,47 +31,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Paw',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme:
-              ColorScheme.fromSeed(seedColor: Color.fromRGBO(254, 201, 255, 1)),
-          textTheme: TextTheme(
-            bodyMedium: TextStyle(fontSize: 16, color: Colors.black),
-            labelLarge: TextStyle(fontSize: 16, color: Colors.black),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.resolveWith((states) {
-                if (states.contains(WidgetState.disabled)) {
-                  return Color.fromRGBO(228, 239, 224, 1);
-                }
-                return Color.fromRGBO(177, 255, 158, 1);
-              }),
-              foregroundColor: WidgetStateProperty.resolveWith((states) {
-                if (states.contains(WidgetState.disabled)) {
-                  return Colors.grey[700];
-                }
-                return Colors.black;
-              }),
-              padding: WidgetStateProperty.all(
-                  EdgeInsets.symmetric(vertical: 16, horizontal: 32)),
-              shape: WidgetStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-                side: BorderSide(
-                  color: Colors.black,
-                  width: 3,
+    return GraphQLProvider(
+        client: client,
+        child: ChangeNotifierProvider(
+          create: (context) => MyAppState(),
+          child: MaterialApp(
+            title: 'Paw',
+            theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(
+                  seedColor: Color.fromRGBO(254, 201, 255, 1)),
+              textTheme: TextTheme(
+                bodyMedium: TextStyle(fontSize: 16, color: Colors.black),
+                labelLarge: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.resolveWith((states) {
+                    if (states.contains(WidgetState.disabled)) {
+                      return Color.fromRGBO(228, 239, 224, 1);
+                    }
+                    return Color.fromRGBO(177, 255, 158, 1);
+                  }),
+                  foregroundColor: WidgetStateProperty.resolveWith((states) {
+                    if (states.contains(WidgetState.disabled)) {
+                      return Colors.grey[700];
+                    }
+                    return Colors.black;
+                  }),
+                  padding: WidgetStateProperty.all(
+                      EdgeInsets.symmetric(vertical: 16, horizontal: 32)),
+                  shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    side: BorderSide(
+                      color: Colors.black,
+                      width: 3,
+                    ),
+                  )),
+                  elevation: WidgetStateProperty.all(0),
                 ),
-              )),
-              elevation: WidgetStateProperty.all(0),
+              ),
             ),
+            home: MyHomePage(),
           ),
-        ),
-        home: MyHomePage(),
-      ),
-    );
+        ));
   }
 }
